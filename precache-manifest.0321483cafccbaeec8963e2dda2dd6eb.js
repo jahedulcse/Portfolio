db84dda22e94e2e5957a2c76b6dd23fc5cbc7de5
JAHEDUL ISLAM <78905127+jahedulcse@users.noreply.github.com>
2022-08-20 15:03:37 +0600

    experience_change

---
+++ precache-manifest.0321483cafccbaeec8963e2dda2dd6eb.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b53c7f895d072b0e6a6c7540d4b5f1e8",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "f7245d56641f025918bf",
+    "url": "/Portfolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "43e86710ce0045a1375f",
+    "url": "/Portfolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "fe884512537d82c00e7a",
+    "url": "/Portfolio/static/css/main.23f75a6c.chunk.css"
+  },
+  {
+    "revision": "df223d00b244486d0c28",
+    "url": "/Portfolio/static/js/2.d948d0dc.chunk.js"
+  },
+  {
+    "revision": "408f8ade359af8695a328ed937291fdf",
+    "url": "/Portfolio/static/js/2.d948d0dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7245d56641f025918bf",
+    "url": "/Portfolio/static/js/3.1bc04481.chunk.js"
+  },
+  {
+    "revision": "43e86710ce0045a1375f",
+    "url": "/Portfolio/static/js/4.a0d26598.chunk.js"
+  },
+  {
+    "revision": "fe884512537d82c00e7a",
+    "url": "/Portfolio/static/js/main.370f4dbc.chunk.js"
+  },
+  {
+    "revision": "476073c5d76e84793f66",
+    "url": "/Portfolio/static/js/runtime-main.5a418774.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "4d5ddb8c3c8c5284fe4961ee5cfa933e",
+    "url": "/Portfolio/static/media/ban_logo.4d5ddb8c.svg"
+  },
+  {
+    "revision": "dc00e29676685236351334c6908fc2e7",
+    "url": "/Portfolio/static/media/city-bank.dc00e296.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/Portfolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/Portfolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/Portfolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "ee62cb58630f29a6e6201b49d4c03728",
+    "url": "/Portfolio/static/media/pwaLogo.ee62cb58.webp"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/Portfolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Portfolio/precache-manifest.607d652ab94bb5fa1e88550ac12fdc88.js"
+  "/Portfolio/precache-manifest.0321483cafccbaeec8963e2dda2dd6eb.js"
 );
 
 self.addEventListener('message', (event) => {
